b4522547e08340a817fc892918bee5932e3fe182
Cosimopyn <pynchon@qq.com>
2018-07-26 13:53:32 +0800

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# sh gene-key.sh first, and add the Raft ID here
+RAFT_ID= 
+
+# Local config
+HOST_IP=172.16.1.4
+PORT=21000
+RPC_PORT=22000
+RAFT_PORT=50401
+CONSTE_PORT=9001
+
+# Cluster config
+CLUSTER_IP=172.16.1.6
+CLUS_CON_PORT=9001
