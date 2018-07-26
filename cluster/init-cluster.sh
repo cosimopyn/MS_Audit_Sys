4eab3cd205aff49d7a6b28dc887765c102ef6f4a
Cosimopyn <pynchon@qq.com>
2018-07-26 22:03:03 +0800

    Update init-cluster.sh

--- cluster/init-cluster.sh
+++ cluster/init-cluster.sh
@@ -9,7 +9,7 @@ mkdir -p qdata/dd1/keystore
 mkdir -p qdata/dd1/geth
 cp permissioned-nodes.json qdata/dd1/static-nodes.json
 cp permissioned-nodes.json qdata/dd1/
-cp keys/key1 qdata/dd1/keystore
+cp key/key1 qdata/dd1/keystore
 cp raft/nodekey1 qdata/dd1/geth/nodekey
 geth --datadir qdata/dd1 init genesis.json
 
@@ -18,7 +18,7 @@ mkdir -p qdata/dd2/keystore
 mkdir -p qdata/dd2/geth
 cp permissioned-nodes.json qdata/dd2/static-nodes.json
 cp permissioned-nodes.json qdata/dd2/
-cp keys/key2 qdata/dd2/keystore
+cp key/key2 qdata/dd2/keystore
 cp raft/nodekey2 qdata/dd2/geth/nodekey
 geth --datadir qdata/dd2 init genesis.json
 
@@ -27,8 +27,8 @@ mkdir -p qdata/dd3/keystore
 mkdir -p qdata/dd3/geth
 cp permissioned-nodes.json qdata/dd3/static-nodes.json
 cp permissioned-nodes.json qdata/dd3/
-cp keys/key6 qdata/dd3/keystore
-cp keys/key3 qdata/dd3/keystore
+cp key/key6 qdata/dd3/keystore
+cp key/key3 qdata/dd3/keystore
 cp raft/nodekey3 qdata/dd3/geth/nodekey
 geth --datadir qdata/dd3 init genesis.json
 
@@ -37,7 +37,7 @@ mkdir -p qdata/dd4/keystore
 mkdir -p qdata/dd4/geth
 cp permissioned-nodes.json qdata/dd4/static-nodes.json
 cp permissioned-nodes.json qdata/dd4/
-cp keys/key4 qdata/dd4/keystore
+cp key/key4 qdata/dd4/keystore
 cp raft/nodekey4 qdata/dd4/geth/nodekey
 geth --datadir qdata/dd4 init genesis.json
 
@@ -45,7 +45,7 @@ echo "[*] Configuring node 5"
 mkdir -p qdata/dd5/keystore
 mkdir -p qdata/dd5/geth
 cp permissioned-nodes.json qdata/dd5/static-nodes.json
-cp keys/key5 qdata/dd5/keystore
+cp key/key5 qdata/dd5/keystore
 cp raft/nodekey5 qdata/dd5/geth/nodekey
 geth --datadir qdata/dd5 init genesis.json
 
@@ -54,7 +54,7 @@ mkdir -p qdata/dd6/keystore
 mkdir -p qdata/dd6/geth
 cp permissioned-nodes.json qdata/dd6/static-nodes.json
 cp raft/nodekey6 qdata/dd6/geth/nodekey
-cp keys/key7 qdata/dd6/keystore
+cp key/key7 qdata/dd6/keystore
 geth --datadir qdata/dd6 init genesis.json
 
 echo "[*] Configuring node 7"
@@ -62,5 +62,5 @@ mkdir -p qdata/dd7/keystore
 mkdir -p qdata/dd7/geth
 cp permissioned-nodes.json qdata/dd7/static-nodes.json
 cp raft/nodekey7 qdata/dd7/geth/nodekey
-cp keys/key8 qdata/dd7/keystore
+cp key/key8 qdata/dd7/keystore
 geth --datadir qdata/dd7 init genesis.json
