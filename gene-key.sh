fe11ed4b70d4b858705ee378d05c09cd96972848
Cosimopyn <pynchon@qq.com>
2018-07-26 13:56:32 +0800

    Update gene-key.sh

--- gene-key.sh
+++ gene-key.sh
@@ -3,16 +3,20 @@
 # Init env
 killall geth bootnode constellation-node
 rm -rf qdata
+sh ./config.sh
+echo "Start to generate keys..."
 
 # Generate keys
 mkdir -p qdata/dd/{keystore,geth}
 mkdir -p qdata/con
 # encrypted Constellation key pair
+echo '----------------------------------------------------------------------------'
 echo 'Now you need to enter your Constellation password'
 constellation-node --generatekeys=qdata/con/tm
 # node key
 bootnode -genkey qdata/dd/geth/nodekey
 # private account key file
+echo '----------------------------------------------------------------------------'
 echo 'Next you need to enter your Etheruem password. Please save it in pw.data file later for login'
 geth --keystore qdata/dd/keystore/ account new
 # enode url
@@ -20,6 +24,7 @@ PUBKEY=`bootnode -nodekey qdata/dd/geth/nodekey -writeaddress`
 ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:${PORT}"'?discport=0&raftport='"${RAFT_PORT}"
 echo Your ENode URL is: $ENODE_URL
 echo 'Please run > raft.addPeer("'$ENODE_URL'") in any node of the blockchain network to get RAFT_ID' 
+echo '----------------------------------------------------------------------------'
 echo 'Done'
 
 mkdir -p qdata/logs
