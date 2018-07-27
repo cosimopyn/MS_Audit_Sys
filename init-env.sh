c869b1eaf2a2b3ac2af9c3c04f852ea064acdf78
Cosimopyn <pynchon@qq.com>
2018-07-27 09:54:16 +0800

    Create init-env.sh

---
+++ init-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+chmod 774 ./cluster/init-cluster.sh
+chmod 774 ./cluster/start-cluster.sh
+chmod 774 ./cluster/stop.sh
+chmod 774 ./constellation-start.sh
+
+chmod 774 ./node/gene-key.sh
+chmod 774 ./node/start-node.sh
