392227d4096b117dbc1854eb42a584eefe02622d
Cosimopyn <pynchon@qq.com>
2018-07-29 19:24:15 +0800

    Update sc2.sol

--- node/sc2.sol
+++ node/sc2.sol
@@ -1,20 +1,26 @@
 pragma solidity ^0.4.15;
 
-contract sc2 {
-  bytes32[] public dataStore;
-  uint public idx;
- 
-  constructor(bytes32 initVal) {
-    dataStore.push(initVal);
-    idx=1;
+contract simple {
+  string[] public _dataStore;
+  string public _info;
+  
+  constructor(string info) public {
+    _info=info;
   }
 
-  function put(bytes32 x) {
-    dataStore.push(x);
-    idx = idx + 1;
+  function put(string record) public {
+    _dataStore.push(record);
   }
 
-  function get() constant returns (bytes32 retVal) {
-    return dataStore[idx];
+  function get_record(uint idx) constant public returns (string retVal) {
+    return _dataStore[idx];
+  }
+  
+  function get_num() constant public returns (uint num) {
+    return _dataStore.length;
+  }
+  
+  function get_info() constant public returns (string info) {
+    return _info;
   }
 }
