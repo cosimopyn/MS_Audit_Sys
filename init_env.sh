8ff8918170c63015897ea9c278c11486a0154108
Cosimopyn <pynchon@qq.com>
2018-07-30 17:47:08 +0800

    Update and rename init-env.sh to init_env.sh

---
+++ init_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+chmod +x ./install_dep.sh
+
+chmod +x ./cluster/init_cluster.sh
+chmod +x ./cluster/start_cluster.sh
+chmod +x ./cluster/start_cluster_permission.sh
+chmod +x ./cluster/stop.sh
+chmod +x ./cluster/constellation_start.sh
+chmod +x ./cluster/gene_enodes.sh
+
+chmod +x ./node/gene_key.sh
+chmod +x ./node/start_node.sh
