24bf5f544ae275f2c1dd80607eba9e0ca612dad6
Cosimopyn <pynchon@qq.com>
2018-07-30 15:04:23 +0800

    Update write_public.sh

--- common/write_public.sh
+++ common/write_public.sh
@@ -13,14 +13,14 @@ then
   #PRIVATE_CONFIG=../node/qdata/con/tm.ipc geth --exec "loadScript(\"public_exist_contract.js\")" attach ipc:../node/qdata/dd/geth.ipc
 else
   sed -i "1d" public_new_contract.js
-  sed -i "var mess=\"$1\";" public_new_contract.js
+  sed -i "1ivar mess=\"$1\";" public_new_contract.js
   
   TXN=`PRIVATE_CONFIG=../cluster/qdata/c2/tm.ipc geth --exec "loadScript(\"public_new_contract.js\")" attach ipc:../cluster/qdata/dd2/geth.ipc`
   sed -i "1d" get_contract_addr.js
   sed -i "1ivar TXNHash=\"$TXN\";" get_contract_addr.js
   
   ADDRESS=
-  DOWN=true
+  DOWN=truech
   ITER=0
   while $DOWN; do
     sleep 0.1
