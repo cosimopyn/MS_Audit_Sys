dabcdd0f621cd1c6f82a67cf3a8b2155aeb2deff
Cosimopyn <pynchon@qq.com>
2018-07-30 17:53:44 +0800

    Update init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -12,6 +12,6 @@ chmod +x ./cluster/gene_enodes.sh
 chmod +x ./node/gene_key.sh
 chmod +x ./node/start_node.sh
 
-chmod +x ./utl/write_public.sh
-chmod +x ./utl/write_private.sh
-chmod +x ./utl/read_data.sh
+chmod +x ./util/write_public.sh
+chmod +x ./util/write_private.sh
+chmod +x ./util/read_data.sh
