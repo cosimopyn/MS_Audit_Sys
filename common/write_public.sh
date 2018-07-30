d6c55b242a9a28102ad45d2bae2545d04dc95422
Cosimopyn <pynchon@qq.com>
2018-07-30 17:28:43 +0800

    Update write_public.sh

--- common/write_public.sh
+++ common/write_public.sh
@@ -4,7 +4,6 @@ TYPE=`jq -r '.TYPE' config-utl.json`
 CON_DD=`jq -r '.CON_DD' config-utl.json`
 QUO_DD=`jq -r '.QUO_DD' config-utl.json`
 
-=cluster
 DATE=`date -d today +"%Y-%m-%d"`
 EXIST_DATE=`tail -n 1 .addresses.dat | cut -b 1-10`
 
