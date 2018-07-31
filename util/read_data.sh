11bae124e67af9e5fb2a5ac9fed51306af589730
Cosimopyn <pynchon@qq.com>
2018-07-31 16:07:57 +0800

    Update read_data.sh

--- util/read_data.sh
+++ util/read_data.sh
@@ -18,7 +18,8 @@ CON_DD=`jq -r '.CON_DD' ./config-util.json`
 QUO_DD=`jq -r '.QUO_DD' ./config-util.json`
 ATTACHPARAMETER="ipc:${QDATA_DIR}/${QUO_DD}/geth.ipc"
 ABI='[{"constant":true,"inputs":[{"name":"idx","type":"uint256"}],"name":"get_record","outputs":[{"name":"retVal","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"get_num","outputs":[{"name":"num","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"record","type":"string"}],"name":"put","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get_info","outputs":[{"name":"info","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_info","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"_dataStore","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"info","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]'
-    
+
+# -peer command
 if [ "$1"x == "-peer"x ]; then
    # PRIVATE_CONFIG=${QDATA_DIR}/${CON_DD}/tm.ipc geth attach $ATTACHPARAMETER <<EOF | grep "Data: " | sed "s/Data: //"
   OUT=`PRIVATE_CONFIG=${QDATA_DIR}/${CON_DD}/tm.ipc geth attach $ATTACHPARAMETER <<EOF
@@ -27,6 +28,8 @@ if [ "$1"x == "-peer"x ]; then
 EOF`
   RES=`echo $OUT  | cut -d '>' -f 2`
   echo "Current peer number (except self) is:$RES"
+  
+# -block command
 elif [ "$1"x == "-block"x ]; then
   OUT=`PRIVATE_CONFIG=${QDATA_DIR}/${CON_DD}/tm.ipc geth attach $ATTACHPARAMETER <<EOF
   eth.blockNumber;
@@ -34,12 +37,16 @@ elif [ "$1"x == "-block"x ]; then
 EOF`
   RES=`echo $OUT  | cut -d '>' -f 2`
   echo "Current block number is:$RES"
+  
+# -data command
 elif [ "$1"x == "-data"x ]; then
-
+  
+  # --day option
   if [ "$2"x == "--day"x ]; then
     echo "day"
-  elif [ "$2"x == "--addr"x ]; then
-  
+    
+  # --addr option
+  elif [ "$2"x == "--addr"x ]; then 
     OUT=`PRIVATE_CONFIG=${QDATA_DIR}/${CON_DD}/tm.ipc geth attach $ATTACHPARAMETER <<EOF
     var simpleContract = web3.eth.contract(${ABI});
     var simple = simpleContract.at("$3");
@@ -73,12 +80,13 @@ EOF`
         echo $RECORD
       done
     fi
-    
-    # echo "Current block number is:$RES"
 
+  # wrong option
   else
     show_help
   fi
+  
+# -help command & wrong command
 else
   show_help
 fi
