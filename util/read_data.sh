939761cc8d77ad292fce7e8d3b906e822184d205
Cosimopyn <pynchon@qq.com>
2018-07-31 09:42:04 +0800

    Update read_data.sh

--- util/read_data.sh
+++ util/read_data.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
-TYPE=`jq -r '.TYPE' config-utl.json`
-CON_DD=`jq -r '.CON_DD' config-utl.json`
-QUO_DD=`jq -r '.QUO_DD' config-utl.json`
+if [ $# -eq 0 ];
+then
+  echo "USAGE:./read_data command [options] [arguments...]"
+  echo ""
+  exit
+fi
+
+TYPE=`jq -r '.TYPE' config-util.json`
+CON_DD=`jq -r '.CON_DD' config-util.json`
+QUO_DD=`jq -r '.QUO_DD' config-util.json`
+ 
+ATTACHPARAMETER="ipc:../$TYPE/qdata/$QUO_DD/geth.ipc"
+
+if [ $1 == "-peer" ];
+then
+   # PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF | grep "Data: " | sed "s/Data: //"
+  OUT=`PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF
+  net.peerCount
+  EOF`
+  RES=`echo $OUT  | cut -d '>' -f 2`
+  echo "Current peer number (except self) is:$RES"
+elif [ $1 == "-block" ];
+then
+  PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF
+  eth.blockNumber;
+  EOF`
+  RES=`echo $OUT  | cut -d '>' -f 2`
+  echo "Current block number is:$RES"
+elif [ $1 == "-data" ];
+then
+  if [ $# -lt 2 ];
+  then
+    
+  else
+    
+  fi
+else
+echo "USAGE:./read_data command [options] [arguments...]"
+fi
