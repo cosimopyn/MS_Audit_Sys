2f1cf36295693c435de5cdc007fbc6d2a6f906e6
Cosimopyn <pynchon@qq.com>
2018-07-31 12:54:45 +0800

    Update constellation_start.sh

--- cluster/constellation_start.sh
+++ cluster/constellation_start.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-HOST_IP=`jq -r '.HOST_IP' config-cluster.json`
-PORT=`jq -r '.PORT' config-cluster.json`
-RAFT_PORT=`jq -r '.RAFT_PORT' config-cluster.json`
-RPC_PORT=`jq -r '.RPC_PORT' config-cluster.json`
-CONSTE_PORT=`jq -r '.CONSTE_PORT' config-cluster.json`
+HOST_IP=`jq -r '.HOST_IP' ./config-cluster.json`
+PORT=`jq -r '.PORT' ./config-cluster.json`
+RAFT_PORT=`jq -r '.RAFT_PORT' ./config-cluster.json`
+RPC_PORT=`jq -r '.RPC_PORT' ./config-cluster.json`
+CONSTE_PORT=`jq -r '.CONSTE_PORT' ./config-cluster.json`
+QDATA_DIR=`jq -r '.QDATA_DIR' ./config-cluster.json`
 
 set -u
 set -e
 
 for i in {1..7}
 do
-    DDIR="qdata/c$i"
+    DDIR="${QDATA_DIR}/c$i"
     mkdir -p $DDIR
-    mkdir -p qdata/logs
+    mkdir -p ${QDATA_DIR}/logs
     cp "keys/tm$i.pub" "$DDIR/tm.pub"
     cp "keys/tm$i.key" "$DDIR/tm.key"
     rm -f "$DDIR/tm.ipc"
@@ -24,8 +25,8 @@ do
     else
     	CMD="constellation-node --url=https://127.0.0.$((i-1)):${CUR_CONSTE_PORT}/ --port=${CUR_CONSTE_PORT} --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://${HOST_IP}:${CONSTE_PORT}/"
     fi
-    echo "$CMD >> qdata/logs/constellation$i.log 2>&1 &"
-    $CMD >> "qdata/logs/constellation$i.log" 2>&1 &
+    echo "$CMD >> ${QDATA_DIR}/logs/constellation$i.log 2>&1 &"
+    $CMD >> "${QDATA_DIR}/logs/constellation$i.log" 2>&1 &
 done
 
 DOWN=true
@@ -34,7 +35,7 @@ while $DOWN; do
     DOWN=false
     for i in {1..7}
     do
-	if [ ! -S "qdata/c$i/tm.ipc" ]; then
+	if [ ! -S "${QDATA_DIR}/c$i/tm.ipc" ]; then
             DOWN=true
 	fi
     done
