df284edbfdaf09c6686cddb6868d498f21b74716
Cosimopyn <pynchon@qq.com>
2018-07-31 12:47:13 +0800

    Update read_data.sh

--- util/read_data.sh
+++ util/read_data.sh
@@ -1,41 +1,55 @@
 #!/bin/bash
 
-if [ $# -eq 0 ];
-then
-  echo "USAGE:./read_data command [options] [arguments...]"
-  echo ""
-  exit
-fi
+function show_help()
+{
+  echo "USAGE: "
+  echo "./read_data command [option] [argument]"
+  echo "            -block                      Get current block number"
+  echo "            -peer                       Get current peer number in the private network (excepet self)"
+  echo "            -help                       Show help"
+  echo "            -data                       Get data in the blockchain"
+  echo "                    --day    <YYYY-MM-DD>      Date"
+  echo "                    --addr   <Address Hash>    Address hash"
+
+}
 
 TYPE=`jq -r '.TYPE' config-util.json`
 CON_DD=`jq -r '.CON_DD' config-util.json`
 QUO_DD=`jq -r '.QUO_DD' config-util.json`
- 
 ATTACHPARAMETER="ipc:../$TYPE/qdata/$QUO_DD/geth.ipc"
 
-if [ $1 == "-peer" ];
-then
+if [ "$1"x == "-peer"x ]; then
    # PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF | grep "Data: " | sed "s/Data: //"
   OUT=`PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF
-  net.peerCount
+  net.peerCount;
+  exit;
   EOF`
   RES=`echo $OUT  | cut -d '>' -f 2`
   echo "Current peer number (except self) is:$RES"
-elif [ $1 == "-block" ];
-then
-  PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF
+elif [ "$1"x == "-block"x ]; then
+  OUT=`PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF
   eth.blockNumber;
+  exit;
   EOF`
   RES=`echo $OUT  | cut -d '>' -f 2`
   echo "Current block number is:$RES"
-elif [ $1 == "-data" ];
-then
-  if [ $# -lt 2 ];
-  then
-    
+elif [ "$1"x == "-data"x ]; then
+  if [ "$2"x == "--day"x ]; then
+    echo "day"
+  elif [ "$2"x == "--addr"x ]; then
+  
+    OUT=`PRIVATE_CONFIG=../$TYPE/qdata/$CON_DD/tm.ipc geth attach $ATTACHPARAMETER << EOF
+    var simpleContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"idx","type":"uint256"}],"name":"get_record","outputs":[{"name":"retVal","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"get_num","outputs":[{"name":"num","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"record","type":"string"}],"name":"put","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get_info","outputs":[{"name":"info","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_info","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"_dataStore","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"info","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]);
+    var simple = simpleContract.at("$3");
+    console.log(simple.get_info());
+    exit;
+    EOF`
+    RES=`echo $OUT  | cut -d '>' -f 4`
+    # echo "Current block number is:$RES"
+
   else
-    
+    show_help
   fi
 else
-echo "USAGE:./read_data command [options] [arguments...]"
+  show_help
 fi
