056d0208cc099d71b06510c2ff1eb1c43ef3f5a1
Cosimopyn <pynchon@qq.com>
2018-07-31 17:07:17 +0800

    Update write_public.sh

--- util/write_public.sh
+++ util/write_public.sh
@@ -11,9 +11,9 @@ fi
 
 DATE=`date -d today +"%Y-%m-%d"`
 EXIST_DATE=`tail -n 1 ${QDATA_DIR}/.addresses.dat | cut -b 1-10`
+ADDRESS=`tail -n 1 ${QDATA_DIR}/.addresses.dat | cut -d " " -f 2`
 
-if [ "$EXIST_DATE"x == "$DATE"x ]; then
-  ADDRESS=`tail -n 1 ${QDATA_DIR}/.addresses.dat | cut -d " " -f 2`
+if [ "$EXIST_DATE"x == "$DATE"x ] && [ "$ADDRESS"x != ""x ]; then
   sed -i -e "s/var mess.*/var mess=\"$1\";/" public_exist_contract.js
   sed -i -e "s/var address.*/var address=\"$ADDRESS\";/" public_exist_contract.js
   OUT=`PRIVATE_CONFIG=${QDATA_DIR}/${CON_DD}/tm.ipc geth --exec "loadScript(\"public_exist_contract.js\")" attach ipc:${QDATA_DIR}/${QUO_DD}/geth.ipc`
