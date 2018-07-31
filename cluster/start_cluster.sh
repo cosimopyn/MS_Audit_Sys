9c25c38454371a6eccc1bf1c4f6ccc8ed4af6a43
Cosimopyn <pynchon@qq.com>
2018-07-31 13:02:43 +0800

    Update start_cluster.sh

--- cluster/start_cluster.sh
+++ cluster/start_cluster.sh
@@ -5,6 +5,7 @@ PORT=`jq -r '.PORT' config-cluster.json`
 RAFT_PORT=`jq -r '.RAFT_PORT' config-cluster.json`
 RPC_PORT=`jq -r '.RPC_PORT' config-cluster.json`
 CONSTE_PORT=`jq -r '.CONSTE_PORT' config-cluster.json`
+QDATA_DIR=`jq -r '.QDATA_DIR' ./config-cluster.json`
 
 echo "Host IP: $HOST_IP"
 echo "Port: $PORT"
@@ -30,21 +31,22 @@ then
 	echo "  1337 is the recommend ChainId for Geth private clients."
 fi
 
-mkdir -p qdata/logs
+mkdir -p ${QDATA_DIR}/logs
 echo "[*] Starting Constellation nodes"
 ./constellation_start.sh
 
 echo "[*] Starting Ethereum nodes with ChainID and NetworkId of $NETWORK_ID"
 set -v
 ARGS="--nodiscover --verbosity 5 --networkid $NETWORK_ID --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
-PRIVATE_CONFIG=qdata/c1/tm.ipc nohup geth --datadir qdata/dd1 $ARGS --raftport $((RAFT_PORT)) --rpcport $((RPC_PORT)) --port $((PORT)) --unlock 0 --password ../pw.dat 2>>qdata/logs/1.log &
-PRIVATE_CONFIG=qdata/c2/tm.ipc nohup geth --datadir qdata/dd2 $ARGS --raftport $((RAFT_PORT+1)) --rpcport $((RPC_PORT+1)) --port $((PORT+1)) --unlock 0 --password ../pw.dat 2>>qdata/logs/2.log &
-PRIVATE_CONFIG=qdata/c3/tm.ipc nohup geth --datadir qdata/dd3 $ARGS --raftport $((RAFT_PORT+2)) --rpcport $((RPC_PORT+2)) --port $((PORT+2)) --unlock 0 --password ../pw.dat 2>>qdata/logs/3.log &
-PRIVATE_CONFIG=qdata/c4/tm.ipc nohup geth --datadir qdata/dd4 $ARGS --raftport $((RAFT_PORT+3)) --rpcport $((RPC_PORT+3)) --port $((PORT+3)) --unlock 0 --password ../pw.dat 2>>qdata/logs/4.log &
-PRIVATE_CONFIG=qdata/c5/tm.ipc nohup geth --datadir qdata/dd5 $ARGS --raftport $((RAFT_PORT+4)) --rpcport $((RPC_PORT+4)) --port $((PORT+4)) --unlock 0 --password ../pw.dat 2>>qdata/logs/5.log &
-PRIVATE_CONFIG=qdata/c6/tm.ipc nohup geth --datadir qdata/dd6 $ARGS --raftport $((RAFT_PORT+5)) --rpcport $((RPC_PORT+5)) --port $((PORT+5)) --unlock 0 --password ../pw.dat 2>>qdata/logs/6.log &
-PRIVATE_CONFIG=qdata/c7/tm.ipc nohup geth --datadir qdata/dd7 $ARGS --raftport $((RAFT_PORT+6)) --rpcport $((RPC_PORT+6)) --port $((PORT+6)) --unlock 0 --password ../pw.dat 2>>qdata/logs/7.log &
+PRIVATE_CONFIG=${QDATA_DIR}/c1/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd1 $ARGS --raftport $((RAFT_PORT)) --rpcport $((RPC_PORT)) --port $((PORT)) --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum1.log &
+PRIVATE_CONFIG=${QDATA_DIR}/c2/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd2 $ARGS --raftport $((RAFT_PORT+1)) --rpcport $((RPC_PORT+1)) --port $((PORT+1)) --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum2.log &
+PRIVATE_CONFIG=${QDATA_DIR}/c3/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd3 $ARGS --raftport $((RAFT_PORT+2)) --rpcport $((RPC_PORT+2)) --port $((PORT+2)) --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum3.log &
+PRIVATE_CONFIG=${QDATA_DIR}/c4/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd4 $ARGS --raftport $((RAFT_PORT+3)) --rpcport $((RPC_PORT+3)) --port $((PORT+3)) --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum4.log &
+PRIVATE_CONFIG=${QDATA_DIR}/c5/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd5 $ARGS --raftport $((RAFT_PORT+4)) --rpcport $((RPC_PORT+4)) --port $((PORT+4)) --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum5.log &
+PRIVATE_CONFIG=${QDATA_DIR}/c6/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd6 $ARGS --raftport $((RAFT_PORT+5)) --rpcport $((RPC_PORT+5)) --port $((PORT+5)) --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum6.log &
+PRIVATE_CONFIG=${QDATA_DIR}/c7/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd7 $ARGS --raftport $((RAFT_PORT+6)) --rpcport $((RPC_PORT+6)) --port $((PORT+6)) --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum7.log &
 set +v
 
 echo
-echo "All nodes configured. See 'qdata/logs' for logs, and run e.g. 'geth attach qdata/dd1/geth.ipc' to attach to the first Geth node."
+echo 'All nodes configured. for logs, see '"${QDATA_DIR}/logs" 
+echo 'To attach to the Geth node, pelase run '"geth attach ${QDATA_DIR}/dd1/geth.ipc" 
