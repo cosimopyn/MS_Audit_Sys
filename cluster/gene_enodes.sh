796a339f85d18d6a05a7b4374ddaba51086de90a
Cosimopyn <pynchon@qq.com>
2018-08-02 15:20:15 +0800

    Update gene_enodes.sh

--- cluster/gene_enodes.sh
+++ cluster/gene_enodes.sh
@@ -16,33 +16,33 @@ ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT))"'?discport=0&raftport='"$((R
 echo "  \"${ENODE_URL}\"," &>>permissioned-nodes.json
 
 PUBKEY=`bootnode -nodekey ${QDATA_DIR}/dd2/geth/nodekey -writeaddress`
-ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+1))"'?discport=0&raftport='"$((RAFT_PORT+1))"
-# ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+1))"'?discport=0&raftport='"$((RAFT_PORT+1))"
+# ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+1))"'?discport=0&raftport='"$((RAFT_PORT+1))"
+ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+1))"'?discport=0&raftport='"$((RAFT_PORT+1))"
 echo "  \"${ENODE_URL}\"," &>>permissioned-nodes.json
 
 PUBKEY=`bootnode -nodekey ${QDATA_DIR}/dd3/geth/nodekey -writeaddress`
-ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+2))"'?discport=0&raftport='"$((RAFT_PORT+2))"
-# ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+2))"'?discport=0&raftport='"$((RAFT_PORT+2))"
+# ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+2))"'?discport=0&raftport='"$((RAFT_PORT+2))"
+ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+2))"'?discport=0&raftport='"$((RAFT_PORT+2))"
 echo "  \"${ENODE_URL}\"," &>>permissioned-nodes.json
 
 PUBKEY=`bootnode -nodekey ${QDATA_DIR}/dd4/geth/nodekey -writeaddress`
-ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+3))"'?discport=0&raftport='"$((RAFT_PORT+3))"
-# ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+3))"'?discport=0&raftport='"$((RAFT_PORT+3))"
+# ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+3))"'?discport=0&raftport='"$((RAFT_PORT+3))"
+ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+3))"'?discport=0&raftport='"$((RAFT_PORT+3))"
 echo "  \"${ENODE_URL}\"," &>>permissioned-nodes.json
 
 PUBKEY=`bootnode -nodekey ${QDATA_DIR}/dd5/geth/nodekey -writeaddress`
-ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+4))"'?discport=0&raftport='"$((RAFT_PORT+4))"
-# ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+4))"'?discport=0&raftport='"$((RAFT_PORT+4))"
+# ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+4))"'?discport=0&raftport='"$((RAFT_PORT+4))"
+ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+4))"'?discport=0&raftport='"$((RAFT_PORT+4))"
 echo "  \"${ENODE_URL}\"," &>>permissioned-nodes.json
 
 PUBKEY=`bootnode -nodekey ${QDATA_DIR}/dd6/geth/nodekey -writeaddress`
-ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+5))"'?discport=0&raftport='"$((RAFT_PORT+5))"
-# ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+5))"'?discport=0&raftport='"$((RAFT_PORT+5))"
+# ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+5))"'?discport=0&raftport='"$((RAFT_PORT+5))"
+ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+5))"'?discport=0&raftport='"$((RAFT_PORT+5))"
 echo "  \"${ENODE_URL}\"," &>>permissioned-nodes.json
 
 PUBKEY=`bootnode -nodekey ${QDATA_DIR}/dd7/geth/nodekey -writeaddress`
-ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+6))"'?discport=0&raftport='"$((RAFT_PORT+6))"
-# ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+6))"'?discport=0&raftport='"$((RAFT_PORT+6))"
+# ENODE_URL='enode://'"${PUBKEY}@127.0.0.1:$((PORT+6))"'?discport=0&raftport='"$((RAFT_PORT+6))"
+ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:$((PORT+6))"'?discport=0&raftport='"$((RAFT_PORT+6))"
 echo "  \"${ENODE_URL}\"" &>>permissioned-nodes.json
 
 echo ']' &>>permissioned-nodes.json
