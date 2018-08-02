5fc9f9f54f7bc50f48840db6ab58889b60ac7330
Cosimopyn <pynchon@qq.com>
2018-08-02 15:58:23 +0800

    Create audit_contract.sol

---
+++ node/audit_contract.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.15;
+
+contract audit_store {
+  string[] public _data;
+  string public _info;
+  
+  constructor(string info) {
+    _info=info;
+  }
+
+  function put(string record) {
+    _data.push(record);
+  }
+
+  function get_record(uint idx) constant returns (string retVal) {
+    return _data[idx];
+  }
+  
+  function get_num() constant public (uint num) {
+    return _data.length;
+  }
+  
+  function get_info() constant public returns (string info) {
+    return _info;
+  }
+}
