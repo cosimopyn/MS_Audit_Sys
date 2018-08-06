033e3acb6e20c2aa49de76f7e1436f1b2b391e4c
Cosimopyn <pynchon@qq.com>
2018-08-06 14:35:52 +0800

    Update write_new_contract.js

--- util/write_new_contract.js
+++ util/write_new_contract.js
@@ -1,5 +1,5 @@
 var mess = "12";
-var cust_addr=0xca843569e3427144cead5e4d5999a3d0ccf92b8e;
+var cust_addr="0xca843569e3427144cead5e4d5999a3d0ccf92b8e";
 
 web3.eth.defaultAccount = eth.accounts[0];
 var abi = [{"constant":true,"inputs":[{"name":"idx","type":"uint256"}],"name":"get_record","outputs":[{"name":"retVal","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"get_num","outputs":[{"name":"num","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"record","type":"string"}],"name":"put","outputs":[{"name":"retVal","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get_info","outputs":[{"name":"info","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"get_customer","outputs":[{"name":"customer","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"info","type":"string"},{"name":"customer","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
