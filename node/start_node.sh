f5386723f0edaa851147fc5f0b0a37bcc39186b5
Cosimopyn <pynchon@qq.com>
2018-08-09 10:07:43 +0800

    Update start_node.sh

--- node/start_node.sh
+++ node/start_node.sh
@@ -80,4 +80,4 @@ echo "customer$((RAFT_ID-7)) $2" &>> ${QDATA_DIR}/.addresses.dat
 
 ACCOUNT=0x`geth --keystore ${QDATA_DIR}/dd/keystore/ account list | cut -d " " -f 3 | cut -b 2-41` 
 CON_PUB_KEY=`cat ${QDATA_DIR}/con/tm.pub`
-echo "[*] To create contract, run on server: ./run.sh -create $((RAFT_ID-7)) $ACCOUNT '$CON_PUB_KEY'"
+echo "[*] To create contract, run on server: ./run.sh -create customer$((RAFT_ID-7)) $ACCOUNT '$CON_PUB_KEY'"
