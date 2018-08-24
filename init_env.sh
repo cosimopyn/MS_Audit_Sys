8577e3c06e18755793d0a68dc04c0446bbd7278e
Cosimopyn <pynchon@qq.com>
2018-08-24 16:58:31 +0800

    Update init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -4,7 +4,6 @@ chmod +x ./install_dep.sh
 
 chmod +x ./cluster/init_cluster.sh
 chmod +x ./cluster/start_cluster.sh
-chmod +x ./cluster/start_cluster_permission.sh
 chmod +x ./cluster/constellation_start.sh
 chmod +x ./cluster/gene_enodes.sh
 
