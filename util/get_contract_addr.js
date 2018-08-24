a002a0f9e41a1c79e46a51e2496c65ed0249ef66
Cosimopyn <pynchon@qq.com>
2018-08-24 10:42:06 +0800

    Update get_contract_addr.js

--- util/get_contract_addr.js
+++ util/get_contract_addr.js
@@ -1,3 +1,10 @@
+/*
+A Java script template to use a transaction hash to get the address of an existing contract
+It is called in the "read_data.func"
+geth --exec "loadScript(\"get_contract_addr.js\") ..."
+which utilizes geth command to attach the node IPC and execute the script
+*/
+
 var TXNHash="";
 
 console.log(eth.getTransactionReceipt(TXNHash).contractAddress);
