462c9deefb1a280f1556fcaf56cc542cce1813da
Cosimopyn <pynchon@qq.com>
2018-08-27 14:53:39 +0800

    Update gene_key.sh

--- node/gene_key.sh
+++ node/gene_key.sh
@@ -7,6 +7,8 @@ HOST_IP=`jq -r '.HOST_IP' ./config.json`
 PORT=`jq -r '.PORT' ./config.json`
 RAFT_PORT=`jq -r '.RAFT_PORT' ./config.json`
 QDATA_DIR=`jq -r '.QDATA_DIR' ./config.json`
+CON_DD=`jq -r '.CON_DD' ./config.json`
+QUO_DD=`jq -r '.QUO_DD' ./config.json`
 
 echo "HOST_IP=$HOST_IP"
 echo "PORT=$PORT"
@@ -19,21 +21,21 @@ rm -rf ${QDATA_DIR}
 
 # Generate keys
 echo "Start to generate keys..."
-mkdir -p ${QDATA_DIR}/dd/keystore
-mkdir -p ${QDATA_DIR}/dd/geth
-mkdir -p ${QDATA_DIR}/c1
+mkdir -p ${QDATA_DIR}/${QUO_DD}/keystore
+mkdir -p ${QDATA_DIR}/${QUO_DD}/geth
+mkdir -p ${QDATA_DIR}/${CON_DD}
 # encrypted Constellation key pair
 echo 'Now you need to enter your Constellation password  (you can use an empty one)'
-constellation-node --generatekeys=${QDATA_DIR}/c1/tm
+constellation-node --generatekeys=${QDATA_DIR}/${CON_DD}/tm
 # node key
-bootnode -genkey ${QDATA_DIR}/dd/geth/nodekey
+bootnode -genkey ${QDATA_DIR}/${QUO_DD}/geth/nodekey
 # private account key file
 echo '----------------------------------------------------------------------------'
 echo 'Next you need to enter your Etheruem password (you can use an empty one)'
 echo '[*] Please remember to save it in \"pw.dat\" file later for login'
-geth --keystore ${QDATA_DIR}/dd/keystore/ account new
+geth --keystore ${QDATA_DIR}/${QUO_DD}/keystore/ account new
 # enode url
-PUBKEY=`bootnode -nodekey ${QDATA_DIR}/dd/geth/nodekey -writeaddress`
+PUBKEY=`bootnode -nodekey ${QDATA_DIR}/${QUO_DD}/geth/nodekey -writeaddress`
 ENODE_URL='enode://'"${PUBKEY}@${HOST_IP}:${PORT}"'?discport=0&raftport='"${RAFT_PORT}"
 echo 'Your ENode URL is:'
 echo "$ENODE_URL"
