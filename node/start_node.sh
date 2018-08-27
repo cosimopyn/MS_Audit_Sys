b1371edf9434c9c1bf6a41d8596f84b83ab20c26
Cosimopyn <pynchon@qq.com>
2018-08-27 14:52:28 +0800

    Update start_node.sh

--- node/start_node.sh
+++ node/start_node.sh
@@ -13,6 +13,8 @@ RAFT_PORT=`jq -r '.RAFT_PORT' ./config.json`
 RPC_PORT=`jq -r '.RPC_PORT' ./config.json`
 CONSTE_PORT=`jq -r '.CONSTE_PORT' ./config.json`
 QDATA_DIR=`jq -r '.QDATA_DIR' ./config.json`
+CON_DD=`jq -r '.CON_DD' ./config.json`
+QUO_DD=`jq -r '.QUO_DD' ./config.json`
 echo "RAFT ID: $RAFT_ID"
 echo "Host IP: $HOST_IP"
 echo "Port: $PORT"
@@ -36,11 +38,11 @@ echo '--------------------------------------------------------------------------
 
 echo "Start to run node..."
 # Init Quorum node
-geth --datadir ${QDATA_DIR}/dd init ../genesis.json
+geth --datadir ${QDATA_DIR}/${QUO_DD} init ../genesis.json
 
 # Start Constellation node
-rm -f ${QDATA_DIR}/con/tm.ipc
-CMD="constellation-node --url=https://$HOST_IP:$CONSTE_PORT/ --port=$CONSTE_PORT --workdir=${QDATA_DIR}/con --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://$CLUSTER_IP:$CLUS_CON_PORT/"
+rm -f ${QDATA_DIR}/${CON_DD}/tm.ipc
+CMD="constellation-node --url=https://$HOST_IP:$CONSTE_PORT/ --port=$CONSTE_PORT --workdir=${QDATA_DIR}/${CON_DD} --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://$CLUSTER_IP:$CLUS_CON_PORT/"
 echo "$CMD >> ${QDATA_DIR}/logs/constellation.log 2>&1 &"
 $CMD >> "${QDATA_DIR}/logs/constellation.log" 2>&1 &
 
@@ -48,7 +50,7 @@ DOWN=true
 while $DOWN; do
     sleep 0.1
     DOWN=false
-    if [ ! -S "${QDATA_DIR}/con/tm.ipc" ]; then
+    if [ ! -S "${QDATA_DIR}/${CON_DD}/tm.ipc" ]; then
         DOWN=true
     fi
 done
@@ -67,14 +69,14 @@ fi
 # Start Quorum node
 ARGS="--nodiscover -verbosity 5 --networkid $NETWORK_ID --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
 # --permissioned
-PRIVATE_CONFIG=${QDATA_DIR}/con/tm.ipc nohup geth --datadir ${QDATA_DIR}/dd $ARGS --raftjoinexisting $RAFT_ID --raftport $RAFT_PORT --rpcport $RPC_PORT --port $PORT --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum.log &
+PRIVATE_CONFIG=${QDATA_DIR}/${CON_DD}/tm.ipc nohup geth --datadir ${QDATA_DIR}/${QUO_DD} $ARGS --raftjoinexisting $RAFT_ID --raftport $RAFT_PORT --rpcport $RPC_PORT --port $PORT --unlock 0 --password ../pw.dat 2>>${QDATA_DIR}/logs/quorum.log &
 echo "Established Quorum node from host($HOST_IP:$PORT) to Network ($CLUSTER_IP)"
 echo '----------------------------------------------------------------------------'
 echo "Done"
-echo 'To attach to the Geth node, pelase run '"geth attach ${QDATA_DIR}/dd/geth.ipc" 
+echo 'To attach to the Geth node, pelase run '"geth attach ${QDATA_DIR}/${QUO_DD}/geth.ipc" 
 
 touch ${QDATA_DIR}/.addresses.dat
 
-ACCOUNT=0x`geth --keystore ${QDATA_DIR}/dd/keystore/ account list | cut -d " " -f 3 | cut -b 2-41` 
-CON_PUB_KEY=`cat ${QDATA_DIR}/con/tm.pub`
+ACCOUNT=0x`geth --keystore ${QDATA_DIR}/${QUO_DD}/keystore/ account list | cut -d " " -f 3 | cut -b 2-41` 
+CON_PUB_KEY=`cat ${QDATA_DIR}/${CON_DD}/tm.pub`
 echo "[*] To create storage, run on server: ./run.sh -create customer$((RAFT_ID-7)) $ACCOUNT '$CON_PUB_KEY'"
